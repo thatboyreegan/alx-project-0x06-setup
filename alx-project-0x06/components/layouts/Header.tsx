ef4f64e3b6997756c39874745ee4583798dba33e
thatboyreegan <reaganronald491@gmail.com>
2025-01-19 16:27:54 +0300

    add: implementing redux state management

--- alx-project-0x06/components/layouts/Header.tsx
+++ alx-project-0x06/components/layouts/Header.tsx
@@ -1,25 +1,40 @@
 import Link from "next/link";
 import Button from "../common/Button";
 import { usePathname } from "next/navigation";
-import { useCount } from "@/context/CountContext";
+import { RootState } from "@/store/store";
+import { useSelector } from "react-redux";
 
 const Header: React.FC = () => {
-  const pathname = usePathname();
-  const { count } = useCount();
+
+  const pathname = usePathname()
+  const count = useSelector((state: RootState) => state.counter.value)
+
 
   return (
-    <header>
-      <div>
-        <Link href="/">Splash App</Link>
+    <header className="fixed w-full bg-white shadow-md">
+      <div className="container mx-auto flex justify-between items-center py-6 px-4 md:px-8">
+        <Link href="/" className="text-3xl md:text-5xl font-bold text-gray-800 tracking-tight">
+          Splash App
+        </Link>
+
+        {/* Button Group */}
         <div className="flex gap-4">
-          {!["/counter-app"].includes(pathname) ? (
-            <>
-              <Button buttonLabel="Sign In" buttonBackgroundColor="red" />
-              <Button buttonLabel="Sign Up" buttonBackgroundColor="blue" />
-            </>
-          ) : (
-            <p className=" font-semibold text-lg">Current count : {count}</p>
-          )}
+          {
+            !["/counter-app"].includes(pathname) ? (
+              <>
+              <Button
+            buttonLabel="Sign In"
+            buttonBackgroundColor="red"
+          />
+          <Button
+            buttonLabel="Sign Up"
+            buttonBackgroundColor="blue"
+          /></>
+            ) : (
+              <p className=" font-semibold text-lg text-black">Current count : {count}</p>
+            )
+          }
+
         </div>
       </div>
     </header>
--- alx-project-0x06/pages/_app.tsx
+++ alx-project-0x06/pages/_app.tsx
@@ -1,14 +1,15 @@
 import Layout from "@/components/layouts/Layout";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import { CountProvider } from "@/context/CountContext";
+import store from "@/store/store";
+import { Provider } from "react-redux";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <CountProvider>
+    <Provider store={store}>
       <Layout>
         <Component {...pageProps} />
       </Layout>
-    </CountProvider>
+    </Provider>
   )
 }
